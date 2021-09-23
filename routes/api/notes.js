c199eb1f2b33291917f1a9b73408b5063e526c32
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-09-23 11:59:43 -0400

    added delete function/ added routes

---
+++ routes/api/notes.js
@@ -0,0 +1,58 @@
+const router = require("express").Router()
+const fs = require("fs/promises")
+
+// ** GET method Route
+router.get("/notes", async (req, res) => {
+    console.log(`${req.method} request for notes receieved`)
+    await fs.readFile("./db/db.json", "utf8").then((data) => res.json(JSON.parse(data)))
+})
+
+// ** POST method ROUTE
+// API end point to get notes from db.json
+router.post("/notes", async (req, res) => {
+    console.log(`POST request recieved to add notes`)
+    let newNotes = {
+        ...req.body,
+        id: Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1),
+    };
+    const data = await fs.readFile("./db/db.json", "utf8")
+    // parse data db.json
+    const parsedNotes = JSON.parse(data)
+
+    // add new notes to db.json
+    parsedNotes.push(newNotes)
+
+    // stringify notes to be saved
+    const noteString = JSON.stringify(parsedNotes, null, 4)
+
+    // saving files to db.json with stringified newNotes
+    await fs.writeFile("./db/db.json", noteString)
+
+    res.json("Successfully added note")
+})
+
+
+//**  DELETE method ROUTE
+router.delete("/notes/:id", async (req, res) => {
+    console.log(`${req.method} request recieved to delete notes`)
+    // Check if there is anything in the response body
+    const data = await fs.readFile("./db/db.json", "utf8")
+    // parse data db.json
+    const parsedNotes = JSON.parse(data)
+
+    //filter notes by id
+    const filteredNote = parsedNotes.filter((note) => note.id !== req.params.id)
+    // stringify notes to be saved
+    const noteString = JSON.stringify(filteredNote)
+
+    // saving files to db.json with stringified newNotes
+    fs.writeFile("./db/db.json", noteString)
+
+
+    res.json(`Note deleted successfully`)
+
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,68 +1,17 @@
-
 const express = require("express")
 const path = require("path")
-const notes = require("./db/db.json")
-const fs = require("fs/promises")
+
+
 const app = express()
-const PORT = 3001
+const PORT = 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
+
 app.use(express.static('public'));
-app.get("/notes", (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
 
-// API end point to get notes from db.json
-app.get("/api/notes", (req, res) => {
-    console.log(`GET request for notes receieved`)
-    res.json(notes)
-})
-
-app.post("/api/notes", (req, res) => {
-    console.log(`POST request recieved to add notes`)
-
-    // Prepare a response object to send back to the client
-    let response;
-
-    // Check if there is anything in the response body
-    if (req.body && req.body.title) {
-        response = {
-            status: 'success',
-            title: req.body,
-            text: req.body,
-        };
-        res.json(`${response.title.title} note has been added!`);
-    } else {
-        res.json('Request error must enter a text and title');
-    }
-
-    // Log the response body to the console
-    console.log(req.body);
-})
-//! I have no idea what I am doing
-
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-
-
-
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-
-
-
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-
-
+app.use(require("./routes"));
 
 
 app.listen(PORT, () => console.log("Listening at http://localhost:" + PORT))
