1c693deafdb2a13f9dc802d287479dff5c2d5ae2
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-09-23 12:00:13 -0400

    cleaned up server.js

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require("express")
-const path = require("path")
-
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
 
 app.use(express.static('public'));
 
